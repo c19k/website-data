1e785825402bc332aa846dbac8474797c9bc115f
Akhil Balakrishnan <akhil.balakrishnan@citrix.com>
2021-08-22 18:23:09 +0530

    Updated constants file

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 const SUMMARY_SHEET = '1tEiOBsvUVz5UcEhwR8Bm4Y4-HFHOVWVZ4AHgAYeRwsg';
-const SHEET_PREFECTURE_TAB = 2
-const SHEET_SUM_BY_DAY_TAB = 3
-const SHEET_LAST_UPDATED_TAB = 4
-const SHEET_AGE_TAB = 5;
-const SHEET_GENDER_TAB = 6
-const SHEET_UNDER_OBSERVATION_TAB = 8;
+const SHEET_PREFECTURE_TAB = "District Data";
+const SHEET_SUM_BY_DAY_TAB = "Sum by Day";
+const SHEET_LAST_UPDATED_TAB = "Last Updated"
+const SHEET_AGE_TAB = "Age";
+const SHEET_GENDER_TAB = "Gender"
+const SHEET_UNDER_OBSERVATION_TAB = "Observation";
 
 const PATIENT_SHEETS = [
     {
