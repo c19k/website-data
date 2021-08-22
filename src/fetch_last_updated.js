f188a31569d2f8a4dbf75d8fe299f3ca0ba6108d
Akhil Balakrishnan <akhil.balakrishnan@citrix.com>
2021-08-22 19:02:56 +0530

    Formatted last updated time #2

--- src/fetch_last_updated.js
+++ src/fetch_last_updated.js
@@ -8,7 +8,7 @@ const SHEET_LAST_UPDATED_TAB = CONSTANTS.SHEET_LAST_UPDATED_TAB;
 
 async function fetchLastUpdated() {
   let lastUpdatedInfo = await FetchSheet.fetchRows(SHEET, SHEET_LAST_UPDATED_TAB)
-  return lastUpdatedInfo[0];
+  return lastUpdatedInfo[0]['lastUpdated'];
 }
 
 exports.fetchLastUpdated = fetchLastUpdated;
